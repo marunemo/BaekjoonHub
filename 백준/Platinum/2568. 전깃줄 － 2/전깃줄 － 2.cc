b70fa906250e7916d859a66becc39c93270dbb92
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-04-26 16:21:55 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 100 ms, Memory: 4432 KB -BaekjoonHub

---
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+using pii = pair<int, int>;
+
+int line_count;
+vector<pii> line;
+vector<int> lis;
+vector<int> order;
+int curr_order;
+vector<int> remove_line;
+
+int main() {
+    int start, end;
+    int left, right, mid;
+
+    cin >> line_count;
+    for(int i = 0; i < line_count; i++) {
+        cin >> start >> end;
+        line.push_back({start, end});
+    }
+    sort(line.begin(), line.end());
+
+    order = vector<int>(line_count);
+    lis.push_back(line[0].second);
+    order[0] = 0;
+    for(int i = 1; i < line_count; i++) {
+        end = line[i].second;
+
+        if(lis.back() < end) {
+            order[i] = lis.size();
+            lis.push_back(end);
+        }
+        else {
+            left = 0;
+            right = lis.size() - 1;
+            while(left < right) {
+                mid = (left + right) / 2;
+                if(lis[mid] <= end)
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+            lis[right] = end;
+            order[i] = right;
+        }
+    }
+    
+    curr_order = lis.size() - 1;
+    for(int i = line_count - 1; i >= 0; i--) {
+        if(order[i] == curr_order)
+            curr_order--;
+        else
+            remove_line.push_back(line[i].first);
+    }
+    sort(remove_line.begin(), remove_line.end());
+
+    cout << (line_count - lis.size()) << '\n';
+    for(auto rm: remove_line)
+        cout << rm << '\n';
+    return 0;
+}
