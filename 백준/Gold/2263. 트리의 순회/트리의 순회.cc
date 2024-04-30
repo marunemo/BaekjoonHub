61c739f6269bcfe8a7f0a4d3b434f92954785b05
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-04-30 17:39:48 +0900

    [Gold I] Title: 트리의 순회, Time: 2136 ms, Memory: 8676 KB -BaekjoonHub

---
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> inorder;
+vector<int> postorder;
+
+void PreOrder(int in_start, int in_end, int post_start, int post_end) {
+    if(in_start > in_end || post_start > post_end)
+        return;
+
+    int root = postorder[post_end];
+    
+    int range;
+    for(range = 0; inorder[in_start + range] != root && in_start + range <= in_end; range++);
+
+    cout << root << ' ';
+    PreOrder(in_start, in_start + range - 1, post_start, post_start + range - 1);
+    PreOrder(in_start + range + 1, in_end, post_start + range, post_end - 1);
+}
+
+int main() {
+    // Fast I/O
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int node_count;
+    cin >> node_count;
+
+    inorder = vector<int>(node_count);
+    for(int i = 0; i < node_count; i++)
+        cin >> inorder[i];
+
+    postorder = vector<int>(node_count);
+    for(int i = 0; i < node_count; i++)
+        cin >> postorder[i];
+    
+    PreOrder(0, node_count - 1 , 0, node_count - 1);
+    return 0;
+}
