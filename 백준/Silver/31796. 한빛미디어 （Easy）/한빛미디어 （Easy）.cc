b6d6ea8a7cb3a1cde96a5d19b572fada734153ff
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-05-05 16:23:30 +0900

    [Silver IV] Title: 한빛미디어 (Easy), Time: 48 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/31796. 한빛미디어 （Easy）/한빛미디어 （Easy）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int book[100000];
+
+int main() {
+    int book_count;
+    int start, count;
+
+    cin >> book_count;
+    for(int i = 0; i < book_count; i++)
+        cin >> book[i];
+    sort(book, book + book_count);
+
+    start = book[0];
+    count = 1;
+    for(int i = 1; i < book_count; i++) {
+        if(start * 2 <= book[i]) {
+            count++;
+            start = book[i];
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
