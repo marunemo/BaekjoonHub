1e2ac6cb170275e7997275b20fc02fb71a998371
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-05-21 12:48:08 +0900

    [Gold III] Title: 당근 클릭 게임, Time: 0 ms, Memory: 3996 KB, Score: 35 point -BaekjoonHub

---
+++ 백준/Gold/31434. 당근 클릭 게임/당근 클릭 게임.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int cost[100];
+int bonus[100];
+int memo[101][5001];
+
+int main() {
+    int n, k;
+    int max_bonus = 0;
+    int max_carrot = 0;
+
+    cin >> n >> k;
+    for(int i = 0; i < n; i++) {
+        cin >> cost[i] >> bonus[i];
+        max_bonus = max(max_bonus, bonus[i]);
+    }
+
+    for(int second = 0; second < k; second++) {
+        for(int carrot = 0; carrot <= max_bonus * k; carrot++)
+            memo[second][carrot] = -1;
+    }
+    memo[0][1] = 0;
+
+    for(int second = 0; second < k; second++) {
+        for(int carrot = 0; carrot <= max_bonus * k; carrot++) {
+            if(memo[second][carrot] == -1)
+                continue;
+
+            memo[second + 1][carrot] = memo[second][carrot] + carrot;
+            for(int item = 0; item < n; item++) {
+                if(memo[second][carrot] >= cost[item])
+                    memo[second + 1][carrot + bonus[item]] = memo[second][carrot] - cost[item];
+            }
+        }
+    }
+
+    for(int carrot = 0; carrot <= max_bonus * k; carrot++)
+        max_carrot = max(max_carrot, memo[k][carrot]);
+    cout << max_carrot << endl;
+    return 0;
+}
