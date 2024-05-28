5effef8b81ef57f00539ce8d0898d51484a7322a
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-05-28 12:00:12 +0900

    [Gold III] Title: 육각 보드, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/12946. 육각 보드/육각 보드.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int map_size;
+char map[50][50];
+int color[50][50] = {0};
+
+int dir[6][2] = {{-1, 0}, {-1, 1}, {0, 1}, {1, 0}, {1, -1}, {0, -1}};
+
+int DFS(int row, int col, int clr) {
+    color[row][col] = clr;
+
+    int count = 1;
+    for(int i = 0; i < 6; i++) {
+        if(row + dir[i][0] >= 0 && row + dir[i][0] < map_size && col + dir[i][1] >= 0 && col + dir[i][1] < map_size) {
+            if(map[row + dir[i][0]][col + dir[i][1]] == 'X') {
+                if(!color[row + dir[i][0]][col + dir[i][1]]) {
+                    count = max(count, 2);
+                    count = max(count, DFS(row + dir[i][0], col + dir[i][1], -clr));
+                }
+                else if(color[row + dir[i][0]][col + dir[i][1]] == clr) {
+                    count = max(count, 3);
+                    return count;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    int count = 0;
+
+    cin >> map_size;
+    for(int row = 0; row < map_size; row++) {
+        for(int col = 0; col < map_size; col++)
+            cin >> map[row][col];
+    }
+
+    for(int row = 0; row < map_size && count < 3; row++) {
+        for(int col = 0; col < map_size && count < 3; col++) {
+            if(map[row][col] == 'X' && !color[row][col])
+                count = max(count, DFS(row, col, 1));
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
