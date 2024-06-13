20f4ec8b9dd4406e47774f388da8e5fa2a3591c5
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-06-13 12:28:06 +0900

    [Platinum V] Title: 찾기, Time: 132 ms, Memory: 10908 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.cc
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -14,29 +14,34 @@ int main() {
     getline(cin, text);
     getline(cin, pattern);
 
+    pattern = " " + pattern;
+
     len = pattern.length();
     prefix.resize(len);
 
-    i = 1;
+    i = 2;
     j = 0;
     prefix[0] = 0;
+    prefix[1] = 0;
     for(; i < len; i++) {
-        while(j > 0 && pattern[i] != pattern[j])
-            j = prefix[j - 1];
+        while(j > 0 && pattern[i] != pattern[j + 1])
+            j = prefix[j];
         
-        if(pattern[i] == pattern[j])
+        if(pattern[i] == pattern[j + 1])
             prefix[i] = ++j;
+        else
+            prefix[i] = 0;
     }
 
     j = 0;
     for(i = 0; i < text.length(); i++) {
-        while(j > 0 && text[i] != pattern[j])
-            j = prefix[j - 1];
+        while(j > 0 && text[i] != pattern[j + 1])
+            j = prefix[j];
         
-        if(text[i] == pattern[j])
+        if(text[i] == pattern[j + 1])
             j++;
 
-        if(j == len)
+        if(j == len - 1)
             index.push_back(i - (j - 1) + 1);
     }
 
