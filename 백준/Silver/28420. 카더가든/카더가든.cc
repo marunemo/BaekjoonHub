20be3c84e6a1472792106f276c54dc84c71972be
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-06-30 11:18:23 +0900

    [Silver I] Title: 카더가든, Time: 20 ms, Memory: 2376 KB -BaekjoonHub

--- 백준/Silver/28420. 카더가든/카더가든.cc
+++ 백준/Silver/28420. 카더가든/카더가든.cc
@@ -1,63 +1,70 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-vector<vector<int>> mask[3];
-vector<vector<int>> map;
+int prefix_sum[301][301] = {0};
 
 int main() {
     int height, width;
     int car_width, car_length, camp_length;
-    int cost, min_cost = 0;
+    int cost, min_cost = 1800000;
 
     cin >> height >> width;
     cin >> car_width >> car_length >> camp_length;
 
-    map = vector<vector<int>>(height, vector<int>(width));
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j++) {
-            cin >> map[i][j];
-            min_cost += map[i][j];
+    for(int i = 1; i <= height; i++) {
+        for(int j = 1; j <= width; j++) {
+            cin >> cost;
+            prefix_sum[i][j] = prefix_sum[i][j - 1] + cost;
         }
+        for(int j = 1; j <= width; j++)
+            prefix_sum[i][j] += prefix_sum[i - 1][j];
     }
-
-    // fig 1.
-    mask[0] = vector<vector<int>>(car_width, vector<int>(car_length + camp_length, 1));
-
-    // fig 2.
-    mask[1] = vector<vector<int>>(car_width + car_length, vector<int>(car_width + camp_length, 0));
-    for(int i = 0; i < car_width; i++) {
-        for(int j = 0; j < camp_length; j++)
-            mask[1][i][j] = 1;
-    }
-    for(int i = 0; i < car_width; i++) {
-        for(int j = 0; j < car_length; j++)
-            mask[1][car_width + j][camp_length + i] = 1;
-    }
-
-    // fig 3.
-    mask[2] = vector<vector<int>>(car_width + camp_length, vector<int>(car_length + car_width, 0));
-    for(int i = 0; i < car_width; i++) {
-        for(int j = 0; j < car_length; j++)
-            mask[2][i][j] = 1;
-    }
-    for(int i = 0; i < car_width; i++) {
-        for(int j = 0; j < camp_length; j++)
-            mask[2][car_width + j][car_length + i] = 1;
-    }
-
+    
     for(int row = 0; row < height; row++) {
         for(int col = 0; col < width; col++) {
-            for(int index = 0; index < 3; index++) {
-                if(row + mask[index].size() > height || col + mask[index][0].size() > width)
-                    continue;
+            // fig 1.
+            if(row + car_width <= height && col + car_length + camp_length <= width) {
+                // car + camp
+                cost = prefix_sum[row + car_width][col + car_length + camp_length];
+                cost -= prefix_sum[row + car_width][col];
+                cost -= prefix_sum[row][col + car_length + camp_length];
+                // 포함 배제의 원리
+                cost += prefix_sum[row][col];
                 
-                cost = 0;
-                for(int i = 0; i < mask[index].size(); i++) {
-                    for(int j = 0; j < mask[index][i].size(); j++)
-                        cost += mask[index][i][j] * map[row + i][col + j];
-                }
+                min_cost = min(cost, min_cost);
+            }
+            
+            // fig 2.
+            if(row + car_width + car_length <= height && col + camp_length + car_width <= width) {
+                // car
+                cost = prefix_sum[row + car_width + car_length][col + camp_length + car_width];
+                cost -= prefix_sum[row + car_width + car_length][col + camp_length];
+                cost -= prefix_sum[row + car_width][col + camp_length + car_width];
+                cost += prefix_sum[row + car_width][col + camp_length];
+
+                // camp
+                cost += prefix_sum[row + car_width][col + camp_length];
+                cost -= prefix_sum[row + car_width][col];
+                cost -= prefix_sum[row][col + camp_length];
+                cost += prefix_sum[row][col];
+                
+                min_cost = min(cost, min_cost);
+            }
+
+            // fig 3.
+            if(row + car_width + camp_length <= height && col + car_length + car_width <= width) {
+                // car
+                cost = prefix_sum[row + car_width][col + car_length];
+                cost -= prefix_sum[row + car_width][col];
+                cost -= prefix_sum[row][col + car_length];
+                cost += prefix_sum[row][col];
+
+                // camp
+                cost += prefix_sum[row + car_width + camp_length][col + car_length + car_width];
+                cost -= prefix_sum[row + car_width + camp_length][col + car_length];
+                cost -= prefix_sum[row + car_width][col + car_length + car_width];
+                cost += prefix_sum[row + car_width][col + car_length];
 
                 min_cost = min(cost, min_cost);
             }
