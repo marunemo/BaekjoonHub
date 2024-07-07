eaf3c818a77b83b2d96d5271e737ff8701956d94
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-07-07 22:36:34 +0900

    [Silver IV] Title: Absolutely Acidic, Time: 528 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/6770. Absolutely Acidic/Absolutely Acidic.cc
+++ 백준/Silver/6770. Absolutely Acidic/Absolutely Acidic.cc
@@ -4,57 +4,45 @@
 #include <algorithm>
 
 using namespace std;
+using pii = pair<int, int>;
 
 int main() {
     int count, value;
-    vector<int> reading;
+    vector<pii> reading;
     unordered_map<int, int> frequency;
-    vector<int> candidate;
-    int max_freq, diff;
-    int largest;
+    int index;
 
+    // input
     cin >> count;
     for(int i = 0; i < count; i++) {
         cin >> value;
 
         if(!frequency[value])
-            reading.push_back(value);
+            reading.push_back({value, 0});
         frequency[value]++;
-    }
-    sort(reading.begin(), reading.end());
 
-    max_freq = 0;
-    for(int value: reading) {
-        if(frequency[value] == max_freq)
-            candidate.push_back(value);
-        else if(frequency[value] > max_freq) {
-            max_freq = frequency[value];
-            candidate = vector<int>({value});
-        }
     }
 
-    if(candidate.size() > 1) {
-        diff = candidate.back() - candidate.front();
-        cout << diff << endl;
-        return 0;
-    }
+    // 각각의 frequency를 reading에 매핑
+    for(int i = 0; i < frequency.size(); i++)
+        reading[i].second = frequency[reading[i].first];
 
-    max_freq = 0;
-    largest = candidate[0];
-    for(int value: reading) {
-        if(value == largest)
-            continue;
-            
-        if(frequency[value] == max_freq)
-            candidate.push_back(value);
-        else if(frequency[value] > max_freq) {
-            max_freq = frequency[value];
-            candidate = vector<int>({value});
-        }
-    }
+    // frequency에 대해 내림차순, 같은 경우 값에 대해 오름차순 정렬
+    sort(reading.begin(), reading.end(), [](pii a, pii b) {
+        if(a.second == b.second)
+            return a.first < b.first;
+        return a.second > b.second;
+    });
 
-    for(int value: candidate)
-        diff = max(diff, abs(largest - value));
-    cout << diff << endl;
+    // 최빈값이 하나가 아닌 경우
+    if(reading[0].second == reading[1].second) {
+        for(index = 1; index < reading.size() && reading[index].second == reading[0].second; index++);
+        cout << reading[index - 1].first - reading[0].first << endl;
+    }
+    // 최빈값이 하나인 경우
+    else {
+        for(index = 2; index < reading.size() && reading[index].second == reading[1].second; index++);
+        cout << max(abs(reading[index - 1].first - reading[0].first), abs(reading[1].first - reading[0].first)) << endl;
+    }
     return 0;
 }
