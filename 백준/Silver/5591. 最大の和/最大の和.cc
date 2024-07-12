17b0992d7bbde5322564b7b78f9d96ba00bcc6d5
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-07-12 20:59:07 +0900

    [Silver IV] Title: 最大の和, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/5591. 最大の和/最大の和.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int num[100000];
+
+int main() {
+    int count, length;
+    int window = 0;
+    int max_window = 0;
+
+    cin >> count >> length;
+    for(int i = 0; i < length; i++) {
+        cin >> num[i];
+        window += num[i];
+    }
+
+    max_window = window;
+    for(int i = length; i < count; i++) {
+        cin >> num[i];
+
+        window -= num[i - length];
+        window += num[i];
+        max_window = max(window, max_window);
+    }
+
+    cout << max_window << endl;
+    return 0;
+}
