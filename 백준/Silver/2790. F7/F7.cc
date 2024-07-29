6385f802670d82215f0cad9ec5818964b36b03b7
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-07-30 00:09:19 +0900

    [Silver II] Title: F7, Time: 52 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/2790. F7/F7.cc
+++ 백준/Silver/2790. F7/F7.cc
@@ -4,28 +4,30 @@
 using namespace std;
 
 int driver[300000];
-int point[300000];
 
 int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
     int n;
     int max_point = 0;
     int count = 0;
     
     cin >> n;
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
         cin >> driver[i];
-        point[i] = n - i;
-    }
     sort(driver, driver + n);
 
     for(int i = 0; i < n; i++)
-        max_point = max(max_point, driver[i] + point[i]);
-    count += (max_point == driver[0] + point[0]);
+        max_point = max(max_point, driver[i] + (n - i));
+    count += (max_point == driver[0] + n);
 
     for(int i = 1; i < n; i++) {
-        point[i] = point[i - 1];
-        max_point = max(max_point, driver[i] + point[i]);
-        count += (max_point == driver[i] + point[i]);
+        if(driver[i] + n >= max_point) {
+            max_point = driver[i] + n;
+            count++;
+        }
     }
 
     cout << count << endl;
