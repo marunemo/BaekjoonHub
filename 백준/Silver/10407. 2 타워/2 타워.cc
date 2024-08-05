a8a28038f4931b449a26e9cc1ab92288dea0b519
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-08-05 19:50:57 +0900

    [Silver III] Title: 2 타워, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10407. 2 타워/2 타워.cc
+++ 백준/Silver/10407. 2 타워/2 타워.cc
@@ -1,11 +1,2 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    char h[102];
-    cin >> h;
-
-    cout << (!h[1] & (h[0] == 49)) + 1 << endl;
-    return 0;
-}
+#include<iostream>
+int main(){char h[102];std::cin>>h;std::cout<<(!h[1]&(h[0]==49))+1;return 0;}
