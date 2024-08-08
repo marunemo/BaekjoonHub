f51fee54d51e0502c51d43b4e4210c4eb3efd6ba
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-08-08 21:48:48 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 0 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
@@ -3,7 +3,7 @@
 using namespace std;
 using ll = long long;
 
-int method[100001][4] = {0};
+int method[100000][3] = {0};
 
 int main() {
     cin.tie(0);
@@ -11,15 +11,15 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     // 1 = 1
-    method[1][1] = 1;
+    method[0][0] = 1;
     // 2 = 2
-    method[2][2] = 1;
+    method[1][1] = 1;
     // 3 = 2 + 1 = 1 + 2 = 3
-    method[3][1] = method[3][2] = method[3][3] = 1;
-    for(int i = 4; i <= 100000; i++) {
-        method[i][1] = ((ll)method[i][1] + method[i - 1][2] + method[i - 1][3]) % 1000000009LL;
-        method[i][2] = ((ll)method[i][2] + method[i - 2][1] + method[i - 2][3]) % 1000000009LL;
-        method[i][3] = ((ll)method[i][3] + method[i - 3][1] + method[i - 3][2]) % 1000000009LL;
+    method[2][0] = method[2][1] = method[2][2] = 1;
+    for(int i = 3; i < 100000; i++) {
+        method[i][0] = ((ll)method[i][0] + method[i - 1][1] + method[i - 1][2]) % 1000000009LL;
+        method[i][1] = ((ll)method[i][1] + method[i - 2][0] + method[i - 2][2]) % 1000000009LL;
+        method[i][2] = ((ll)method[i][2] + method[i - 3][0] + method[i - 3][1]) % 1000000009LL;
     }
 
     int tc, len;
@@ -27,7 +27,7 @@ int main() {
     cin >> tc;
     while(tc--) {
         cin >> len;
-        cout << ((ll)method[len][1] + method[len][2] + method[len][3]) % 1000000009LL << '\n';
+        cout << ((ll)method[len - 1][0] + method[len - 1][1] + method[len - 1][2]) % 1000000009LL << '\n';
     }
     return 0;
 }
