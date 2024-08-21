3928dea5551d127eaed5cb5abc167ad35ff97290
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-08-21 15:31:48 +0900

    [Gold IV] Title: 정산소, Time: 28 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/11735. 정산소/정산소.cc
+++ 백준/Gold/11735. 정산소/정산소.cc
@@ -3,32 +3,29 @@
 using namespace std;
 using ll = long long;
 
-ll n, q;
+int n, q;
 char dir;
-ll num, sum;
-ll r_offset = 0, r_count = 0;
-bool r_contain[1000001];
-ll c_offset = 0, c_count = 0;
-bool c_contain[1000001];
+int num;
+ll sum, r_offset, c_offset;
+int r_count, c_count;
+bool r_contain[1000001], c_contain[1000001];
 
 int main() {
     cin.tie(0);
     cout.tie(0);
     ios_base::sync_with_stdio(false);
 
-
     cin >> n >> q;
     while(q--) {
         cin >> dir >> num;
         
-        // 행/열에서 누적으로 합해지는 부분 + 증가하는 부분
-        sum = num * n + n * (n + 1) / 2;
+        sum = (ll)num * n + (ll)n * (n + 1) / 2;
 
         if(dir == 'R') {
             if(r_contain[num])
                 sum = 0;
             else {
-                sum -= num * c_count + c_offset;
+                sum -= (ll)num * c_count + c_offset;
 
                 r_offset += num;
                 r_count++;
@@ -39,7 +36,7 @@ int main() {
             if(c_contain[num])
                 sum = 0;
             else {
-                sum -= num * r_count + r_offset;
+                sum -= (ll)num * r_count + r_offset;
 
                 c_offset += num;
                 c_count++;
