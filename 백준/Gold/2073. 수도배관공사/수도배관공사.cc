f09e3536f3d565db6a1e7e17b5299214eac6149d
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-09-08 16:55:57 +0900

    [Gold IV] Title: 수도배관공사, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2073. 수도배관공사/수도배관공사.cc
+++ 백준/Gold/2073. 수도배관공사/수도배관공사.cc
@@ -1,24 +1,25 @@
 #include <iostream>
 
 using namespace std;
-using Pipe = pair<int, int>;
 
-Pipe pipe[351];
 int knapsack[100001] = {0};
 
 int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
     int distance, pipe_count;
+    int pipe_len, pipe_cap;
 
     cin >> distance >> pipe_count;
-    for(int i = 1; i <= pipe_count; i++)
-        cin >> pipe[i].first >> pipe[i].second;
-    
     for(int i = 1; i <= pipe_count; i++) {
-        for(int length = distance; length > pipe[i].first; length--) {
-            if(knapsack[length - pipe[i].first])
-                knapsack[length] = max(knapsack[length], min(pipe[i].second, knapsack[length - pipe[i].first]));
+        cin >> pipe_len >> pipe_cap;
+        for(int length = distance; length > pipe_len; length--) {
+            if(knapsack[length - pipe_len])
+                knapsack[length] = max(knapsack[length], min(pipe_cap, knapsack[length - pipe_len]));
         }
-        knapsack[pipe[i].first] = max(knapsack[pipe[i].first], pipe[i].second);
+        knapsack[pipe_len] = max(knapsack[pipe_len], pipe_cap);
     }
 
     cout << knapsack[distance] << endl;
