738da46450cd395891de06b2fbcf45e3e8609614
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-09-08 16:53:52 +0900

    [Gold IV] Title: 수도배관공사, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2073. 수도배관공사/수도배관공사.cc
+++ 백준/Gold/2073. 수도배관공사/수도배관공사.cc
@@ -14,12 +14,11 @@ int main() {
         cin >> pipe[i].first >> pipe[i].second;
     
     for(int i = 1; i <= pipe_count; i++) {
-        for(int length = distance; length > 0; length--) {
-            if(length == pipe[i].first)
-                knapsack[length] = max(knapsack[length], pipe[i].second);
-            else if(length > pipe[i].first && knapsack[length - pipe[i].first])
+        for(int length = distance; length > pipe[i].first; length--) {
+            if(knapsack[length - pipe[i].first])
                 knapsack[length] = max(knapsack[length], min(pipe[i].second, knapsack[length - pipe[i].first]));
         }
+        knapsack[pipe[i].first] = max(knapsack[pipe[i].first], pipe[i].second);
     }
 
     cout << knapsack[distance] << endl;
