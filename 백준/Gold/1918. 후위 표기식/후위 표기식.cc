d4f61a77c644b1456a27b8102a4672f4b4545575
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-10-01 00:01:39 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string formula;
+    vector<string> _operand;
+    vector<char> _operator;
+    string first, second;
+
+    cin >> formula;
+    for(char op: formula) {
+        if(op == '(')
+            _operator.push_back(op);
+        else if(op == ')') {
+            while(!_operator.empty() && _operator.back() != '(') {
+                second = _operand.back();
+                _operand.pop_back();
+                first = _operand.back();
+                _operand.pop_back();
+
+                _operand.push_back(first + second + _operator.back());
+                _operator.pop_back();
+            }
+            if(!_operator.empty() && _operator.back() == '(')
+                _operator.pop_back();
+        }
+        else if(op == '*' || op == '/') {
+            if(_operator.empty() || _operator.back() == '(')
+                _operator.push_back(op);
+            else if(_operator.back() == '*' || _operator.back() == '/') {
+                second = _operand.back();
+                _operand.pop_back();
+                first = _operand.back();
+                _operand.pop_back();
+
+                _operand.push_back(first + second + _operator.back());
+                _operator.pop_back();
+
+                _operator.push_back(op);
+            }
+            else if(_operator.back() == '+' || _operator.back() == '-')
+                _operator.push_back(op);
+
+        }
+        else if(op == '+' || op == '-') {
+            while(!_operator.empty() && _operator.back() != '(') {
+                second = _operand.back();
+                _operand.pop_back();
+                first = _operand.back();
+                _operand.pop_back();
+
+                _operand.push_back(first + second + _operator.back());
+                _operator.pop_back();
+            }
+
+            _operator.push_back(op);
+        }
+        else {
+            string tmp(1, op);
+            _operand.push_back(tmp);
+        }
+    }
+    while(!_operator.empty() && _operator.back() != '(') {
+        second = _operand.back();
+        _operand.pop_back();
+        first = _operand.back();
+        _operand.pop_back();
+
+        _operand.push_back(first + second + _operator.back());
+        _operator.pop_back();
+    }
+
+    cout << _operand.back() << endl;
+    return 0;
+}
