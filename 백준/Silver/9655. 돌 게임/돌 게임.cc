56949797700a17c87c1f279b296bc0c37899d898
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-10-14 01:56:38 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -4,13 +4,9 @@ using namespace std;
 
 int main() {
     int n;
-    bool sk_win[1001] = {false, true, false, true};
 
     cin >> n;
-    for(int i = 4; i <= n; i++)
-        sk_win[i] = (!sk_win[i - 1]) || (!sk_win[i - 3]);
-    
-    if(sk_win[n])
+    if(n % 2)
         cout << "SK" << endl;
     else
         cout << "CY" << endl;
