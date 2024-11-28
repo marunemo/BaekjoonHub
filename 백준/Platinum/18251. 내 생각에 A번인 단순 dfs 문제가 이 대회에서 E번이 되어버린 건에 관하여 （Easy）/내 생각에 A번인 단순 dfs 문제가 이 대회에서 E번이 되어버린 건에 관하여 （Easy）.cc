d41ca1d868621688da73fc7b9238af35723e5d8a
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-11-29 00:22:46 +0900

    [Platinum IV] Title: 내 생각에 A번인 단순 dfs 문제가 이 대회에서 E번이 되어버린 건에 관하여 (Easy), Time: 68 ms, Memory: 6128 KB -BaekjoonHub

--- 백준/Platinum/18251. 내 생각에 A번인 단순 dfs 문제가 이 대회에서 E번이 되어버린 건에 관하여 （Easy）/내 생각에 A번인 단순 dfs 문제가 이 대회에서 E번이 되어버린 건에 관하여 （Easy）.cc
+++ 백준/Platinum/18251. 내 생각에 A번인 단순 dfs 문제가 이 대회에서 E번이 되어버린 건에 관하여 （Easy）/내 생각에 A번인 단순 dfs 문제가 이 대회에서 E번이 되어버린 건에 관하여 （Easy）.cc
@@ -2,21 +2,23 @@
 #include <cmath>
 
 using namespace std;
-using ll = long long;
-// depth & weight
-using TreeNode = pair<int, ll>;
 
-int node_count;
-int node[262144];
-int flatten_idx = 1;
-TreeNode flatten[262144];
+int node_count = 0;
+int node[262144] = {0};
+int inorder_idx = 1;
+int level[262144] = {0};
+long long weight[262144] = {0};
 
 void InOrder(int node_idx, int depth) {
     if(node_idx > node_count)
         return;
     
     InOrder(node_idx * 2, depth + 1);
-    flatten[flatten_idx++] = {depth, node[node_idx]};
+
+    level[inorder_idx] = depth;
+    weight[inorder_idx] = node[node_idx];
+    inorder_idx++;
+
     InOrder(node_idx * 2 + 1, depth + 1);
 }
 
@@ -26,35 +28,26 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     int height;
-    ll sum, max_sum = -1e9;
+    long long sum, max_sum = -1e9;
 
     cin >> node_count;
     height = log2(node_count + 1);
 
-    for(int i = 1; i <= node_count; i++) {
+    for(int i = 1; i <= node_count; i++)
         cin >> node[i];
-        max_sum = max(max_sum, (ll)node[i]);
-    }
     InOrder(1, 0);
 
-    if(max_sum < 0) {
-        cout << max_sum << endl;
-        return 0;
-    }
-
-    max_sum = 0;
     for(int top = 0; top < height; top++) {
         for(int bottom = top; bottom < height; bottom++) {
             sum = 0;
             for(int i = 1; i <= node_count; i++) {
-                if(flatten[i].first < top || flatten[i].first > bottom)
+                if(level[i] < top || level[i] > bottom)
                     continue;
                 
-                sum += flatten[i].second;
+                sum += weight[i];
+                max_sum = max(sum, max_sum);
                 if(sum < 0)
                     sum = 0;
-                else
-                    max_sum = max(sum, max_sum);
             }
         }
     }
