326c0cb342ca6ffebb32464649a6f59d08f7ff9d
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-11-29 10:31:50 +0900

    [Silver II] Title: 베르트랑 공준, Time: 36 ms, Memory: 2860 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+bool IsPrime(int num) {
+    for(int i = 2; i * i <= num; i++) {
+        if(num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int prime_count[123456 * 2  + 1] = {0};
+    int n;
+    
+    for(int num = 2; num <= 123456 * 2; num++)
+        prime_count[num] = IsPrime(num) + prime_count[num - 1];
+    
+    cin >> n;
+    while(n) {
+        cout << (prime_count[n * 2] - prime_count[n]) << '\n';
+        cin >> n;
+    }
+    return 0;
+}
