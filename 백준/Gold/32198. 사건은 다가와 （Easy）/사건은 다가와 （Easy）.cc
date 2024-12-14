a9c36f6706b2daa1c5713cb20a369c752206bc35
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-12-14 12:43:17 +0900

    [Gold V] Title: 사건은 다가와 (Easy), Time: 12 ms, Memory: 9852 KB -BaekjoonHub

---
+++ 백준/Gold/32198. 사건은 다가와 （Easy）/사건은 다가와 （Easy）.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#define OFFSET 1000
+
+using namespace std;
+using pii = pair<int, int>;
+
+pii bodybang[1001];
+int DP[1001][2001];
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int n, t, a, b;
+    int finish_time = 0;
+    int min_move;
+
+    cin >> n;
+    while(n--) {
+        cin >> t >> a >> b;
+        bodybang[t] = {a + OFFSET, b + OFFSET};
+
+        if(t > finish_time)
+            finish_time = t;
+    }
+
+    for(int i = -1000 + OFFSET; i <= 1000 + OFFSET; i++)
+        DP[0][i] = -1;
+    DP[0][0 + OFFSET] = 0;
+
+    for(int time = 1; time <= finish_time; time++) {
+        for(int i = -1000 + OFFSET; i <= 1000 + OFFSET; i++) {
+            DP[time][i] = -1;
+            if(bodybang[time].first < i && i < bodybang[time].second)
+                continue;
+            
+            if(i > -1000 + OFFSET && DP[time - 1][i - 1] != -1) {
+                if(DP[time][i] == -1)
+                    DP[time][i] = DP[time - 1][i - 1] + 1;
+                else
+                    DP[time][i] = min(DP[time][i], DP[time - 1][i - 1] + 1);
+            }
+
+            if(DP[time - 1][i] != -1) {
+                if(DP[time][i] == -1)
+                    DP[time][i] = DP[time - 1][i];
+                else
+                    DP[time][i] = min(DP[time][i], DP[time - 1][i]);
+            }
+
+            if(i < 1000 + OFFSET && DP[time - 1][i + 1] != -1) {
+                if(DP[time][i] == -1)
+                    DP[time][i] = DP[time - 1][i + 1] + 1;
+                else
+                    DP[time][i] = min(DP[time][i], DP[time - 1][i + 1] + 1);
+            }
+        }
+    }
+
+    min_move = finish_time + 1;
+    for(int i = -1000 + OFFSET; i <= 1000 + OFFSET; i++) {
+        if(DP[finish_time][i] != -1)
+            min_move = min(min_move, DP[finish_time][i]);
+    }
+    
+    if(min_move == finish_time + 1)
+        cout << -1 << endl;
+    else
+        cout << min_move << endl;
+    return 0;
+}
