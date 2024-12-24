3250eabcf176fd3aea2a33371798837eafa89773
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-12-24 11:04:49 +0900

    [Gold IV] Title: DDR 체력 관리, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/29756. DDR 체력 관리/DDR 체력 관리.cc
+++ 백준/Gold/29756. DDR 체력 관리/DDR 체력 관리.cc
@@ -5,7 +5,7 @@ using namespace std;
 int phase_count, heal;
 int score[1001];
 int damage[1001];
-int DP[1001][101] = {0};
+int DP[101] = {0};
 
 int main() {
     cin.tie(0);
@@ -22,14 +22,16 @@ int main() {
     
     for(int i = 1; i <= phase_count; i++) {
         for(int j = 100; j >= 0; j--)
-            DP[i][min(100, j + heal)] = max(DP[i][min(100, j + heal)], DP[i - 1][j]);
+            DP[min(100, j + heal)] = max(DP[min(100, j + heal)], DP[j]);
+        for(int j = 0; j < heal; j++)
+            DP[j] = 0;
 
         for(int j = damage[i]; j <= 100; j++)
-            DP[i][j - damage[i]] = max(DP[i][j - damage[i]], DP[i][j] + score[i]);
+            DP[j - damage[i]] = max(DP[j - damage[i]], DP[j] + score[i]);
     }
     
     for(int i = 0; i <= 100; i++)
-        max_score = max(max_score, DP[phase_count][i]);
+        max_score = max(max_score, DP[i]);
     cout << max_score << endl;
     return 0;
 }
