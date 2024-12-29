454fb1add10aa74767bc0fe503ebe404a3821a23
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2024-12-29 16:07:24 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.cc
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.cc
@@ -4,24 +4,21 @@ using namespace std;
 
 int main() {
     char pixel;
-    int idx;
 
     for(int i = 0; i < 15 * 15; i++) {
         cin >> pixel;
-        if(pixel == 'w')
-            idx = 1;
-        else if(pixel == 'b')
-            idx = 2;
-        else if(pixel == 'g')
-            idx = 3;
+        if(pixel == 'w') {
+            cout << "chunbae" << endl;
+            return 0;
+        }
+        else if(pixel == 'b') {
+            cout << "nabi" << endl;
+            return 0;
+        }
+        else if(pixel == 'g') {
+            cout << "yeongcheol" << endl;
+            return 0;
+        }
     }
-
-    if(idx == 1)
-        cout << "chunbae";
-    else if(idx == 2)
-        cout << "nabi";
-    else if(idx == 3)
-        cout << "yeongcheol";
-    cout << endl;
     return 0;
 }
