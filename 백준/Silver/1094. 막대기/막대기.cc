e9bfc2787939233020a7698500e4ace468fa9cb1
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2025-02-07 22:13:41 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    
+    cin >> n;
+    while(n) {
+        count += n & 1;
+        n >>= 1;
+    }
+    cout << count << endl;
+    return 0;
+}
