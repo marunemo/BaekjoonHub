37a3b365defec0be29c3c3c2a1393bd07c3ed0b8
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2025-02-12 19:27:38 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,15 @@
+x = int(input())
+if 620 <= x <= 780:
+    print("Red")
+if 590 <= x < 620:
+    print("Orange")
+if 570 <= x < 590:
+    print("Yellow")
+if 495 <= x < 570:
+    print("Green")
+if 450 <= x < 495:
+    print("Blue")
+if 425 <= x < 450:
+    print("Indigo")
+if 380 <= x < 425:
+    print("Violet")
