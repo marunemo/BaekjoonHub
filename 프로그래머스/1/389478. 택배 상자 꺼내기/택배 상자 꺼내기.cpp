a031a4247472dc61a07f4f82a459f4f69c3bead9
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2025-02-13 15:26:06 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int w, int num) {
+    int answer = 0;
+    
+    while(num <= n) {
+        if(num % w == 0)
+            num++;
+        else
+            num += (w - (num % w)) * 2 + 1;        
+        answer++;
+    }
+    
+    return answer;
+}
