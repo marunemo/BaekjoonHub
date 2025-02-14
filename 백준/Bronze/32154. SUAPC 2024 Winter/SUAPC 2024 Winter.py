c0e51b976035f7e6af8c2310fecfb11254e06520
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2025-02-14 16:23:19 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -1,14 +1,2 @@
-x={
-    1: "ABCDEFGHJLM",
-    2: "ACEFGHILM",
-    3: "ACEFGHILM",
-    4: "ABCEFGHLM",
-    5: "ACEFGHLM",
-    6: "ACEFGHLM",
-    7: "ACEFGHLM",
-    8: "ACEFGHLM",
-    9: "ACEFGHLM",
-    10: "ABCFGHLM",
-}[int(input())]
-print(len(x))
-print(*x)
+x={1:"ABCDEFGHJLM",2:"ACEFGHILM",3:"ACEFGHILM",4:"ABCEFGHLM",5:"ACEFGHLM",6:"ACEFGHLM",7:"ACEFGHLM",8:"ACEFGHLM",9:"ACEFGHLM",10:"ABCFGHLM"}[int(input())]
+print(len(x));print(*x)
