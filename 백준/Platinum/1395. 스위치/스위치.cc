08ef86a9657ca96d75df5fe782717c64a896e80d
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2025-03-11 13:51:24 +0900

    [Platinum III] Title: 스위치, Time: 80 ms, Memory: 3300 KB -BaekjoonHub

--- 백준/Platinum/1395. 스위치/스위치.cc
+++ 백준/Platinum/1395. 스위치/스위치.cc
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 
 using namespace std;
 
-vector<int> seg_tree;
-vector<bool> lazy;
+int seg_tree[1 << 18];
+bool lazy[1 << 18];
 
 void CheckLazy(int start, int end, int node) {
     if(!lazy[node]) {
@@ -24,21 +22,19 @@ void CheckLazy(int start, int end, int node) {
 
 int UpdateTree(int start, int end, int node, const int &left, const int &right) {
     CheckLazy(start, end, node);
-
+    
     if(left <= start && end <= right) {
         lazy[node] = !lazy[node];
         CheckLazy(start, end, node);
-        return seg_tree[node];
+    }
+    else if(left <= end && start <= right) {
+        int mid = start + (end - start) / 2;
+        int left_node = UpdateTree(start, mid, node * 2, left, right);
+        int right_node = UpdateTree(mid + 1, end, node * 2 + 1, left, right);
+        seg_tree[node] = left_node + right_node;
     }
 
-    if(end < left || right < start) {
-        return seg_tree[node];
-    }
-
-    int mid = start + (end - start) / 2;
-    int left_node = UpdateTree(start, mid, node * 2, left, right);
-    int right_node = UpdateTree(mid + 1, end, node * 2 + 1, left, right);
-    return seg_tree[node] = left_node + right_node;
+    return seg_tree[node];
 }
 
 int QueryTree(int start, int end, int node, const int &left, const int &right) {
@@ -62,17 +58,12 @@ int main() {
     cin.tie(0);
     cout.tie(0);
     ios_base::sync_with_stdio(false);
-    
+
     int n, m;
     int height;
     int query, s, t;
 
     cin >> n >> m;
-
-    height = ceil(log2(n));
-    seg_tree.resize(1 << (1 + height));
-    lazy.resize(1 << (1 + height));
-
     while(m--) {
         cin >> query >> s >> t;
 
